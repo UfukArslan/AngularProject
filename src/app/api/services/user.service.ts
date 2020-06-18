6baeecd2d1867c32f747377452f1a92fcf268cd8
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-06-18 21:43:51 +0200

    project-starter - finish

--- src/app/api/services/user.service.ts
+++ src/app/api/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "src/app/models/user";
+import { environment} from "../../../environments/environment"
 
 // TODO: Insert here your personnal api URL
 const apiUrl = "<REPLACE_ME>";
@@ -13,6 +14,6 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   loadAllUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${apiUrl}/users`);
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
   }
 }
