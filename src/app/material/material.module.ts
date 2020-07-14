9da4a3c0c9e6e9e7ad2e42b27e34518073c3d1e1
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-07-14 17:02:50 +0200

    AngulatMaterial LoginPage Title

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 
 const MaterialComponents =[
   MatButtonModule,
-  MatSliderModule
+  MatSliderModule,
+  MatCardModule,
+  MatInputModule
 ];
 
 @NgModule({
