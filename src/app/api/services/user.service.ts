3c0081906094207aa8803544baf2da359d0eb8e3
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-07-18 23:45:53 +0200

    user.service.ts => registerNew()

--- src/app/api/services/user.service.ts
+++ src/app/api/services/user.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "src/app/models/user";
 import { environment} from "../../../environments/environment"
+import { AuthRequest } from 'src/app/models/auth-request';
 
 // TODO: Insert here your personnal api URL
 const apiUrl = "<REPLACE_ME>";
@@ -15,5 +16,9 @@ export class UserService {
 
   loadAllUsers(): Observable<User[]> {
     return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
+
+  registerNew(newUser: AuthRequest): Observable<AuthRequest> {
+    return this.http.post<AuthRequest>(`${environment.apiUrl}/users`, newUser);
   }
 }
