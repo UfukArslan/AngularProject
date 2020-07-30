b248815d5776898221b28fe080a45eddf29e61d4
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-07-30 22:37:18 +0200

    mat-list-item function

--- src/app/places-map/places-map.component.ts
+++ src/app/places-map/places-map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService } from '../security/auth.service';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-places-map',
@@ -9,9 +11,14 @@ export class PlacesMapComponent implements OnInit {
 
   opened: boolean;
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigateByUrl("/login");
   }
 
 }
